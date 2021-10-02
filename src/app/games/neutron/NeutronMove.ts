83ec39a59b55d0808f8c8c6bfb3fbfc45fbe4e31
QGI <xquentin_gille@hotmail.com>
2021-10-02 18:17:52 +0200

    NeutronMove Update

--- src/app/games/neutron/NeutronMove.ts
+++ src/app/games/neutron/NeutronMove.ts
@@ -1,48 +1,45 @@
 import { Coord } from 'src/app/jscaip/Coord';
-import { Direction } from 'src/app/jscaip/Direction';
-import { NumberEncoder } from 'src/app/jscaip/Encoder';
-import { MoveCoordToCoord } from 'src/app/jscaip/MoveCoordToCoord';
-import { MGPFallible } from 'src/app/utils/MGPFallible';
-import { JSONValue } from 'src/app/utils/utils';
-import { NeutronState } from './NeutronState';
+import { MoveEncoder } from 'src/app/jscaip/Encoder';
+import { MoveCoord } from 'src/app/jscaip/MoveCoord';
+import { MGPOptional } from 'src/app/utils/MGPOptional';
+import { JSONObject, JSONValueWithoutArray } from 'src/app/utils/utils';
 
-export class NeutronMove extends MoveCoordToCoord {
-    public static encoder: NumberEncoder<NeutronMove> =
-        MoveCoordToCoord.getEncoder<NeutronMove>(NeutronState.SIZE, NeutronState.SIZE,
-                                                       (start: Coord, end: Coord): NeutronMove => {
-                                                           return NeutronMove.of(start, end).get();
-                                                       });
+export class NeutronMove extends MoveCoord {
 
-    public static of(start: Coord, end: Coord): MGPFallible<NeutronMove> {
-        const directionOptional: MGPFallible<Direction> = Direction.factory.fromMove(start, end);
-        if (directionOptional.isFailure()) {
-            return MGPFallible.failure(directionOptional.getReason());
+    public static encoder: MoveEncoder<NeutronMove> = new class extends MoveEncoder<NeutronMove> {
+        public encodeMove(move: NeutronMove): JSONValueWithoutArray {
+            const encoded: JSONValueWithoutArray = {
+                coord: Coord.encoder.encode(move.coord),
+            };
+
+            return encoded;
         }
-        if (start.isNotInRange(NeutronState.SIZE, NeutronState.SIZE)) {
-            return MGPFallible.failure('start coord is not in range');
+        public decodeMove(encoded: JSONValueWithoutArray): NeutronMove {
+            const casted: JSONObject = encoded as JSONObject;
+
+            const coord: Coord = Coord.encoder.decode(casted['coord']);
+            
+            return NeutronMove.move(coord.x, coord.y);
         }
-        if (end.isNotInRange(NeutronState.SIZE, NeutronState.SIZE)) {
-            return MGPFallible.failure('end coord is not in range');
-        }
-        return MGPFallible.success(new NeutronMove(start, end, directionOptional.get()));
     }
-    private constructor(start: Coord, end: Coord, public readonly direction: Direction) {
-        super(start, end);
-        this.direction = Direction.factory.fromMove(start, end).get();
+    public static move(x: number, y: number): NeutronMove {
+        return new NeutronMove(x, y);
     }
-    public equals(o: NeutronMove): boolean {
-        if (o === this) return true;
-        if (!o.coord.equals(this.coord)) return false;
-        return o.end.equals(this.end);
+    private constructor(x: number,
+                        y: number)
+    {
+        super(x, y);
+        if (this.coord.isNotInRange(5, 5)) {
+            throw new Error('The board is a 6 cas wide square, invalid coord: ' + this.coord.toString());
+        }
     }
     public toString(): string {
-        return 'NeutronMove(' + this.coord + '->' + this.end + ')';
+        return 'NeutronMove' + this.coord.toString();
     }
-    public encode(): JSONValue {
-        return NeutronMove.encoder.encode(this);
+    public equals(o: NeutronMove): boolean {
+        if (this.coord.equals(o.coord) === false) {
+            return false;
+        }
+        return this.coord.equals(o.coord);
     }
-    public decode(encodedMove: JSONValue): NeutronMove {
-        return NeutronMove.encoder.decode(encodedMove);
-    }
-
 }
